04d871623d555610c84f465477114b29898f9694
CrazyJoey4 <crazyjoeyooi@gmail.com>
2023-07-01 05:28:35 +0800

    update dynamic active class

--- moviePage.php
+++ moviePage.php
@@ -1,28 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css"
-        integrity="sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="web.css">
-
     <title> Movie </title>
-    <link rel = "icon" href = "media/movie-icon.png">
+    <link rel="icon" href="media/movie-icon.png">
 
+    <?php
+    include "./navBar.php";
+    ?>
 </head>
 
 <body>
     <?php
     echo '<div class="wrap">';
-
-    include "./navBar.php";
-    
-    echo'
+    echo '
         <!-- Search Container -->
         <div class="search-container">
             <div class="search-element">
@@ -51,9 +42,7 @@
     </div>
     
     <!-- movie app js -->
-    <script src="movieAPI.js">
-
-    </script>
+    <script src="movieAPI.js"></script>
     ';
     ?>
 </body>
