07f2b43ff29ee9a49172b97565c80d3b1a72f919
Teriyaki <103395356+PauThing@users.noreply.github.com>
2023-07-01 23:01:56 +0800

    sign in

--- signinPage.php
+++ signinPage.php
@@ -2,28 +2,51 @@
 <html>
 
 <head>
+<meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
     <title> Sign In </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.css"
+        integrity="sha512-Z0kTB03S7BU+JFU0nw9mjSBcRnZm2Bvm0tzOX9/OuOuz01XQfOpa0w/N9u6Jf2f1OAdegdIPWZ9nIZZ+keEvBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="icon" href="media/login-icon.png">
     <link rel="stylesheet" href="sign.css">
 </head>
 
 <body>
     <?php
-    echo '<div class="wrap">';
     echo '
-        <!-- Search Container -->
-        <div class="search-container">
-            <form id="search-form">
-                <div class="search-element">
-                    <h4>Search News:</h4>
-                    <input type="text" class="form-control" placeholder="Search news keyword here..." id="news-search-box">
-                </div>
-            </form>
-        </div>
+    <div class="login-block">
+        <form method="post" action="signinCheck.php" class="login-form">
+            <div class="container">
+                <h1>- Midaya<span>Hub -</span></h1>
+                <h2 style="text-transform: uppercase;">Sign In</h2>
+            </div>
 
-        <br/>
+            <br/><br/>
 
-        <main></main>
+            <div class="wrap">
+                <form>
+                    <div class="InputText">
+                        <input type="text" name = "User_ID" id = "User_ID" required>
+                        <label>ID</label>
+                    </div>
+                        
+                    <br><br><br>
+                        
+                    <div class="InputText">
+                        <input type="password" name="User_pwd" id="User_pwd" required>
+                        <label>Password</label>
+                    </div>
+                    
+                    <br><br><br>
+                    
+                    <input type="submit" name="Sign" id="Sign" value="Log In" class="submit">
+                    <br>						
+                </form>
+            </div>	
+        </form>
     </div>
     
     <!-- news app js -->
